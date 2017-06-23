b4e3c01bb994145249ff34c6c851fe8118d0cd89
olivier2012 <olivier20120424@gmail.com>
2017-06-24 06:01:54 +0800

    1, --first session and channel - by jsch - login  ----- inital_linux ---

---
+++ src/jl/inital_linux.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jl;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.HostKey;
+import com.jcraft.jsch.HostKeyRepository;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.UIKeyboardInteractive;
+import com.jcraft.jsch.UserInfo;
+import static java.awt.SystemColor.info;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Base64;
+import java.util.Properties;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author olivier-h
+ */
+public class inital_linux {
+
+    public static void main(String[] args) throws JSchException, IOException {
+         try {
+        JSch jsch = new JSch();
+
+        /*如果是新的ip 可能 会遇到 UnknownHostKey ， 需要处理 */
+        jsch.setKnownHosts("C:\\Users\\olivier-h\\.ssh\\known_hosts");
+        String host = null;
+        String username = null;
+        String passwd = null;
+
+        //System.out.print("please input the host that you want to connect (ip address : 127.0.0.1): ");
+        host = "192.168.2.204";
+        username = "olivier";
+        passwd = "x09seokw";
+
+        Session session = jsch.getSession(username, host, 22);
+        
+        session.setPassword(passwd);
+        
+//        String passphrase = null;
+        UserInfo ui = new MyUserInfo();
+        session.setUserInfo(ui);
+       session.connect(30000);
+        Channel channel = session.openChannel("shell");
+        String command = "top";
+        channel.setInputStream(System.in);
+        channel.setOutputStream(System.out);
+        channel.connect(3 * 1000); 
+        /* set channelExec  for run  some command  at linux host side */
+
+        
+        /*绑定输入的 缓存在 这个 channelExec 上 ，然后可以 在远端 输入任何命令 
+        InputStream in = channelExec.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String line;
+        while((line = reader.readLine()) !=null){
+          System.out.println("please input the command : " + line);
+        }
+        
+        int exitStatus = channelExec.getExitStatus();
+        if(exitStatus > 0){
+          System.out.println("remote exec error ! " + exitStatus);
+        }*/
+        Thread.sleep(1000);
+//        session.disconnect();
+        } catch (Exception ee) {
+            System.out.println(ee);
+        }
+    }
+}
+
